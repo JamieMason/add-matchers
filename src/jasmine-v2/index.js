20a25452c0e865c571242236a6b5a507458795b4
Jamie Mason <jamie@foldleft.io>
2017-01-18 20:34:36 +0000

    feat(jasmine): add adapter for jasmine 2.x

---
+++ src/jasmine-v2/index.js
@@ -0,0 +1,35 @@
+var matcherFactory = require('./matcherFactory');
+var memberMatcherFactory = require('./memberMatcherFactory');
+
+module.exports = {
+  getAdapters: function (globals) {
+    return {
+      1: createFactory(getAdapter(1)),
+      2: createFactory(getAdapter(2)),
+      3: createFactory(getAdapter(3)),
+      4: createFactory(getAdapter(4))
+    };
+
+    function createFactory(adapter) {
+      return function (name, matcher) {
+        var matchersByName = {};
+        matchersByName[name] = adapter(name, matcher);
+        globals.beforeEach(function () {
+          globals.jasmine.addMatchers(matchersByName);
+        });
+        return matchersByName;
+      };
+    }
+
+    function getAdapter(argsCount) {
+      return function (name, matcher) {
+        var factory = isMemberMatcher(name) ? memberMatcherFactory : matcherFactory;
+        return factory[argsCount](name, matcher);
+      };
+    }
+
+    function isMemberMatcher(name) {
+      return name.search(/^toHave/) !== -1;
+    }
+  }
+};
